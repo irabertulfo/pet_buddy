949d77221c2ba42f96d1b069ee17e41a5ce1776c
Aldrin Niell M. Basa <aldrinbasa@yahoo.com>
2023-07-23 06:09:09 +0800

    [FEATURE] Page transition - push left

--- lib/utils/page_transition.dart
+++ lib/utils/page_transition.dart
@@ -23,4 +23,27 @@ class PageTransition {
       ),
     );
   }
+
+  static void pushRightNavigation(BuildContext context, Widget page) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => page,
+        transitionDuration: const Duration(milliseconds: 500),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0.0); // Push from the right
+          const end = Offset.zero;
+          const curve = Curves.easeOutQuart;
+
+          final tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
 }
