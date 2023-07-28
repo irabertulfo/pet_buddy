995074bed2421d05890d1e095eb40a0bd9ec0532
Aldrin Niell M. Basa <aldrinbasa@yahoo.com>
2023-07-28 15:02:20 +0800

    [FIX] Admin Drawer

--- lib/view/home/admin/admin_home_client.dart
+++ lib/view/home/admin/admin_home_client.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pet_buddy/model/user_model.dart';
 import 'package:pet_buddy/utils/firebase_storage.dart';
 import 'package:pet_buddy/view/home/admin/admin_tab_bar.dart';
+import 'package:pet_buddy/view/home/client/about-us/about_us_screen.dart';
+import 'package:pet_buddy/view/home/client/profile-photo.dart';
 
 class AdminHomeScreen extends StatefulWidget {
   const AdminHomeScreen({Key? key}) : super(key: key);
@@ -39,6 +41,61 @@ class AdminHomeScreenState extends State<AdminHomeScreen> {
         ),
       ),
       body: const AdminTabBar(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            FutureBuilder<String>(
+              future: imageUrlFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return DrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else {
+                  final imageUrl = snapshot.data;
+                  return DrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: ProfilePicture(
+                      imageUrl: imageUrl ?? "",
+                      name: '${user!.firstName} ${user?.lastName}',
+                    ),
+                  );
+                }
+              },
+            ),
+            ListTile(
+              title: const Text('About Us'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutUsScreen()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Logout'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
